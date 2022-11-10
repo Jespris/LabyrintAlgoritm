7e46f8c946c03e59ba430c7bc523ee85dade7dcc
Jesper Andersson <87135834+Jespris@users.noreply.github.com>
2022-11-10 19:50:54 +0200

    Fixed spelling mistakes

--- src/Ex1.java
+++ src/Ex1.java
@@ -16,33 +16,31 @@ public class Ex1 {
     private static final int WIDTH = 800;  // Size of the window in pixels
     private static final int HEIGHT = 800;
     
-    static int cells=100;    // The size of the maze is cells*cells (default is 20*20)
+    static int CELLS_IN_ROW = 100;    // The size of the maze is cells*cells (default is 20*20)
     
     public static void main(String[] args) {
 	
 		// Get the size of the maze from the command line
 		if (args.length > 0) {
 			try {
-				cells = Integer.parseInt(args[0]);  // The maze is of size cells*cells
+				CELLS_IN_ROW = Integer.parseInt(args[0]);  // The maze is of size cells*cells
 			} catch (NumberFormatException e) {
 				System.err.println("Argument " + args[0] + " should be an integer");
 				System.exit(-1);
 			}
 		}
 		// Check that the size is valid
-		if ( (cells <= 1) || (cells > 100) ) {
+		if ( (CELLS_IN_ROW <= 1) || (CELLS_IN_ROW > 100) ) {
 			System.err.println("Invalid size, must be between 2 and 100 ");
 			System.exit(-1);
 		}
-		Runnable r = new Runnable() {
-			public void run() {
-				// Create a JComponent for the maze
-				MazeComponent mazeComponent = new MazeComponent(WIDTH, HEIGHT, cells);
-				// Change the text of the OK button to "Close"
-				UIManager.put("OptionPane.okButtonText", "Close");
-				JOptionPane.showMessageDialog(null, mazeComponent, "Maze " + cells + " by " + cells,
-							  JOptionPane.INFORMATION_MESSAGE);
-			}
+		Runnable r = () -> {
+			// Create a JComponent for the maze
+			MazeComponent mazeComponent = new MazeComponent(WIDTH, HEIGHT, CELLS_IN_ROW);
+			// Change the text of the OK button to "Close"
+			UIManager.put("OptionPane.okButtonText", "Close");
+			JOptionPane.showMessageDialog(null, mazeComponent, "Maze " + CELLS_IN_ROW + " by " + CELLS_IN_ROW,
+						  JOptionPane.INFORMATION_MESSAGE);
 		};
 		SwingUtilities.invokeLater(r);
 	}
@@ -77,7 +75,7 @@ class MazeComponent extends JComponent {
 		for (int i = 0; i<=cells; i++) {        // Draw horizontal grid lines
 			g.drawLine (0, i*cellHeight, cells*cellWidth, i*cellHeight);
 		}
-		for (int j = 0; j<=cells; j++) {       // Draw verical grid lines
+		for (int j = 0; j<=cells; j++) {       // Draw vertical grid lines
 			g.drawLine (j*cellWidth, 0, j*cellWidth, cells*cellHeight);
 		}
 
@@ -189,29 +187,29 @@ class MazeComponent extends JComponent {
 	}
 
 
-	// Paints the interior of the cell at postion x,y with colour c
+	// Paints the interior of the cell at position x,y with colour c
     private void paintCell(int x, int y, Color c, Graphics g) {
-		int xpos = x*cellWidth;    // Position in pixel coordinates
-		int ypos = y*cellHeight;
+		int x_pos = x*cellWidth;    // Position in pixel coordinates
+		int y_pos = y*cellHeight;
 		g.setColor(c);
-		g.fillRect(xpos+1, ypos+1, cellWidth-1, cellHeight-1);
+		g.fillRect(x_pos+1, y_pos+1, cellWidth-1, cellHeight-1);
     }
 
     
     // Draw the wall w in cell (x,y) (0=left, 1=up, 2=right, 3=down)
     private void drawWall(int col, int row, int wallSide, Graphics g) {
-		int xpos = col*cellWidth;    // Position in pixel coordinates
-		int ypos = row*cellHeight;
+		int x_pos = col*cellWidth;    // Position in pixel coordinates
+		int y_pos = row*cellHeight;
 
 		switch (wallSide) {
 			case (0) ->       // Wall to the left
-					g.drawLine(xpos, ypos + 1, xpos, ypos + cellHeight - 1);
+					g.drawLine(x_pos, y_pos + 1, x_pos, y_pos + cellHeight - 1);
 			case (1) ->       // Wall at top
-					g.drawLine(xpos + 1, ypos, xpos + cellWidth - 1, ypos);
+					g.drawLine(x_pos + 1, y_pos, x_pos + cellWidth - 1, y_pos);
 			case (2) ->      // Wall to the right
-					g.drawLine(xpos + cellWidth, ypos + 1, xpos + cellWidth, ypos + cellHeight - 1);
+					g.drawLine(x_pos + cellWidth, y_pos + 1, x_pos + cellWidth, y_pos + cellHeight - 1);
 			case (3) ->      // Wall at bottom
-					g.drawLine(xpos + 1, ypos + cellHeight, xpos + cellWidth - 1, ypos + cellHeight);
+					g.drawLine(x_pos + 1, y_pos + cellHeight, x_pos + cellWidth - 1, y_pos + cellHeight);
 		}
     }
 }
